8dd5c631711cead5d96819d1fdddb7afb934e4d0
wdmd2022 <doug@dougrules.com>
2022-09-01 13:31:56 -0700

    upload 5 without null string check

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if not my_string:
-        return
     stringtoreturn = ""
     for i in range(len(my_string)):
         if my_string[i] == "c" or my_string[i] == "C":
