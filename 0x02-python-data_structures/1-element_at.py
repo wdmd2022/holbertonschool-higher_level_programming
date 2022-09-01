68a69f909d3cbed9cc8daafa10acf13eb4cc6ed6
wdmd2022 <doug@dougrules.com>
2022-09-01 11:46:56 -0700

    upload 1

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx > len(my_list):
-        return
+        return None
     if idx < 0:
-        return
+        return None
     return my_list[idx]
