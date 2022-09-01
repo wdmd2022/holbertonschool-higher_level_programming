c63ddeec7a2467a1b6458b7c3e1f33768aef7511
wdmd2022 <doug@dougrules.com>
2022-09-01 15:49:29 -0700

    upload 9

---
+++ 0x02-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    biggest = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > biggest:
+            biggest = my_list[i]
+    return biggest
