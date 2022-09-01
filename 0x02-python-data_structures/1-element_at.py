3d6a2e9a12784fbe84a1f925939a38bdaf2b4280
wdmd2022 <doug@dougrules.com>
2022-09-01 11:51:05 -0700

    upload 1 but account for requests with index equal to length instead of just greater than length as both are out of bounds

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list):
-        return None
-    if idx < 0:
+    if idx >= len(my_list) or idx < 0:
         return None
     return my_list[idx]
