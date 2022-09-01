3c686832521a5af2fc371f421eaa26138ff2b3de
wdmd2022 <doug@dougrules.com>
2022-09-01 12:07:39 -0700

    upload 3 but account for empty list)

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) = 0:
-        return(my_list)
+        return
     for i in range(len(my_list)):
         print("{:d}".format(my_list[len(my_list) - 1 - i]))
