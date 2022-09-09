4c0afbf1624fc0fbc290cd19290d162c09f7f115
wdmd2022 <doug@dougrules.com>
2022-09-09 12:32:01 -0700

    upload 0 with infinity errors

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer(add_integer(1, 2), add_integer(4, 5)))
-"""print(add_integer(5))
+print(add_integer(float('inf'), float('-inf')))
+"""print(add_integer(add_integer(1, 2), add_integer(4, 5)))
+print(add_integer(5))
 print(add_integer(2))
 print(add_integer(100.3, -2))
 try:
