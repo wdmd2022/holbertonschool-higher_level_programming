646b3652b8f5937009bb66d7a5e698eb76033d9c
wdmd2022 <doug@dougrules.com>
2022-09-12 16:33:21 -0700

    upload 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""this module contains a function to print a square
+"""
+
+def print_square(size):
+    """this function prints a square w/ side length size
+
+    Args:
+        size (int): length of the side
+
+    Raises:
+        TypeError: if size is not an int
+        ValueError: if size is < 0
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for row in range(size):
+        print("#" * size)
