9218e1979e26082ccd88ed16afbf7fddd3314d8d
wdmd2022 <doug@dougrules.com>
2022-09-12 20:41:10 -0700

    upload 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,13 +3,26 @@
 """
 
 def text_indentation(text):
+    """this function formats a long string w/ newlines at '.' ':' or '?'
+
+    Args:
+        text (str): the string to be formatted
+
+    Raises:
+        TypeError: if text is not a string
+    """
     if type(text) != str:
         raise TypeError("text must be a string")
     flag = 0
     for character in text:
-        print("{}".format(character), end="")
+        if flag != 1:
+            if character == " ":
+                continue
+            else:
+                flag = 1
         if flag == 1:
-            print("\n" + "\n")
-            flag = 0
-        if character == "." or "?" or ":":
-            flag = 1
+            if character == "." or character == "?" or character == ":":
+                print("{}{}".format(character, "\n"))    
+                flag = 0
+            else:
+                print("{}".format(character), end="")
