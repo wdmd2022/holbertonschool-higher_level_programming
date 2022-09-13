79cf518add25ac4f2e291eece886b5c443286857
wdmd2022 <doug@dougrules.com>
2022-09-13 13:44:41 -0700

    upload with two more tests for 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,10 +17,13 @@ def add_integer(a, b=98):
     Returns:
         int: the sum of addition
     """
-    if type(a) != int and type(a) != float:
+    if a is None or (type(a) != int and type(a) != float):
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
     if type(a) == float:
         a = int(a)
     if type(b) == float:
