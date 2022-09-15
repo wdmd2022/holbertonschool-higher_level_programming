6ee01bc5f46e761a5672e73d9b4146faaf702e12
wdmd2022 <doug@dougrules.com>
2022-09-15 16:47:59 -0700

    upload 0 with pep8 styling

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """this module contains a function to return info about an object type"""
 
+
 def lookup(obj):
     """returns the attributes and methods of a given object"""
     return dir(obj)
