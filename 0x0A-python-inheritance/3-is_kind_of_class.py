8fc3c2e5ecf2993549018f66ab2f2fb4eb9da192
wdmd2022 <doug@dougrules.com>
2022-09-15 18:21:57 -0700

    simplify last function

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,7 +6,4 @@ def is_kind_of_class(obj, a_class):
     """returns true if obj is an instance of class a_class or a
     type that inherits from a_class
     """
-    if type(obj) == a_class or issubclass(obj, a_class):
-        return True
-    else:
-        return False
+    return isinstance(obj, a_class)
