3dfb327d0c2c827f4f47ca3d07179423601898c0
wdmd2022 <doug@dougrules.com>
2022-09-15 18:26:05 -0700

    upload 4 again

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,6 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj inherits from a_class
-    """
+    """returns true if obj inherits from a_class"""
     return issubclass(obj, a_class)
