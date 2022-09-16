473023ac29f71e691edd1f66eea4fa59d60a527b
wdmd2022 <doug@dougrules.com>
2022-09-15 20:24:23 -0700

    upload 7 with pep8 styling

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,6 +7,7 @@ class BaseGeometry:
     def area(self):
         """this method raises an exception"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """this method validates an integer"""
         if type(value) != int:
