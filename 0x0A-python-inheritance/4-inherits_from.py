564184fc5cc2f58e527583aca78f8f6137e33322
wdmd2022 <doug@dougrules.com>
2022-09-15 18:28:23 -0700

    upload 4 with type syntax

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """returns true if obj inherits from a_class"""
-    return (issubclass(obj, a_class) and type(obj) != a_class)
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
