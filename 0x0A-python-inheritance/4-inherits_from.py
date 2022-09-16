7ad5964c0ab9f8986cc10f220fa67ecaf248dc8e
wdmd2022 <doug@dougrules.com>
2022-09-15 18:25:00 -0700

    upload 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module contains the function inherits_from"""
+
+
+def inherits_from(obj, a_class):
+    """returns true if obj inherits from a_class
+    """
+    return issubclass(obj, a_class)
