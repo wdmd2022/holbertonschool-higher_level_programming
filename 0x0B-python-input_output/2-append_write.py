ea84af59629c519bb13f08ca066b8aff6720c20e
wdmd2022 <doug@dougrules.com>
2022-09-19 14:27:23 -0700

    upload 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a function that appends text to a file"""
+
+
+def append_file(filename="", text=""):
+    """this function appends a string to a file"""
+    with open(filename, mode="a", encoding='UTF-8') as mr_file:
+        charswritten = mr_file.write(text)
+        return charswritten
