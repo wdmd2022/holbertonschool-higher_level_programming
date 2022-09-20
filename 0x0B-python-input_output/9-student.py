0b46f9b6ab83c132ee8b97b8c71b4719e918f56a
wdmd2022 <doug@dougrules.com>
2022-09-19 19:51:05 -0700

    upload student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,13 +2,13 @@
 """this module contains a class Student"""
 
 
-class Student(first_name, last_name, age):
+class Student:
     """this class represents a Student object"""
     def __init__(self, first_name, last_name, age):
         """initializes the Student"""
-        Student.first_name = first_name
-        Student.last_name = last_name
-        Student.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
     def to_json(self):
         """retrieves a dictionary representation of a Student"""
-        return Student.__dict__
+        return self.__dict__
