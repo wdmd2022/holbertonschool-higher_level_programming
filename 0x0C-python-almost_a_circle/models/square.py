ee8cace7afc17081143853f22b484909f029c0d7
wdmd2022 <doug@dougrules.com>
2022-09-23 09:13:34 -0700

    upload square but don't include self when using super class init

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     """this defines the Square class"""
     def __init__(self, size, x=0, y=0, id=None):
         """this initializes the Square"""
-        super().__init__(self, size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         twine = "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
