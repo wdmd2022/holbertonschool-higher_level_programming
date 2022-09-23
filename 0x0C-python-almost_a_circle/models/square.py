44a00991c39cbf9676108d540f15e11dd4b3b9aa
wdmd2022 <doug@dougrules.com>
2022-09-23 09:05:25 -0700

    upload square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this module contains a Square (shaped, not termperaent) class"""
+
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        """this initializes the Square"""
+        super().__init__(self, size, size, x, y, id)
+
+    def __str__(self):
+        twine = "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
+                                                          self.y, self.height)
+        return twine
