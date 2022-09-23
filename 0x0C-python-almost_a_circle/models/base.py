081497db5da6d731e639ad38e6ab58f0f57076f1
wdmd2022 <doug@dougrules.com>
2022-09-23 13:39:56 -0700

    upload list dictionaries string thing

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """this module contains the Base class"""
 
+import json
+
 
 class Base():
     """this class represents the Base class we will use"""
@@ -13,3 +15,11 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns a JSON string rep of a list of dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
