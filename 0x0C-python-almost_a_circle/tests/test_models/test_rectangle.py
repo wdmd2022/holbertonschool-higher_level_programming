037cbd5f32cb5f215d593042f503120331c7eaac
wdmd2022 <doug@dougrules.com>
2022-09-23 21:22:32 -0700

    upload more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """this module contains unit tests for the rectangle module"""
 import unittest
+from models.base import Base
 from models import rectangle
 from models.rectangle import Rectangle
 
@@ -19,5 +20,13 @@ class TestRectangle(unittest.TestCase):
         """you gotta have docs"""
         self.assertTrue(len(Rectangle.__init__.__doc__) > 0)
 
+    def test_rec_id(self):
+        """test giving rectangle an id"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(1, 1)
+        r2 = Rectangle(2, 2)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.id, 2)
+        
 if __name__ == "__main__":
     unittest.main()
