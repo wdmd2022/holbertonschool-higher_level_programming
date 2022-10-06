fbc9371977fdafe4e1758fe09b9f0e0738fc6824
Douglas <wdmdavison@icloud.com>
2022-10-05 21:30:45 -0700

    remove brackets

--- 0x00-SQL_introduction/1-create_database_if_missing.sql
+++ 0x00-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- now I am getting pretty experienced and this is my second script
 -- so let's create a database if it doesn't exist yet
-CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
