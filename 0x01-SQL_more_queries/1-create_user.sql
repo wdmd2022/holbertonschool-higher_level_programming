ecd51ecf6d79a53a517d2957b09dd3e84dcde4c4
Douglas <wdmdavison@icloud.com>
2022-10-06 19:09:48 -0700

    change backticks to single quotes

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- now let's create a user (if they don't already exist) on our server,
 -- with all privileges, if he doesn't exist, with name user_0d_1 and pass user_0d_1_pwd
-CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
