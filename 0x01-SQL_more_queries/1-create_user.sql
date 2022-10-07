97061a5b2063223c8a5df02dee60305282381ff1
Douglas <wdmdavison@icloud.com>
2022-10-06 19:00:23 -0700

    include grant option

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- now let's create a user (if they don't already exist) on our server,
 -- with all privileges, if he doesn't exist, with name user_0d_1 and pass user_0d_1_pwd
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost` WITH GRANT OPTION;
 FLUSH PRIVILEGES;
