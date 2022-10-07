de632515fd169a8d1860cf77a9887bc37cfa363e
Douglas <wdmdavison@icloud.com>
2022-10-06 19:17:39 -0700

    upload 2 again

--- 0x01-SQL_more_queries/2-create_read_user.sql
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- create the user user_0d_2 (if they don't exist)
 -- the user should have pass 'user_0d_2_pwd'
 -- they should have SELECT privilege in the database
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* to 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* to 'user_0d_2'@'localhost';
