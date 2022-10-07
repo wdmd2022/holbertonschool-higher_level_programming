d9086d1674c1f3e0f1fb688f68fe24e4c3f09ed5
Douglas <wdmdavison@icloud.com>
2022-10-06 20:17:34 -0700

    correct quotes

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 -- states table contains only one record where name = California (but the id can
 -- be different). Sort results by ascending cities.id, don't use the JOIN command
 SELECT id, name FROM cities
-WHERE state_id = (SELECT id from states WHERE name = "California")
+WHERE state_id = (SELECT id from states WHERE name = 'California')
 ORDER BY id;
