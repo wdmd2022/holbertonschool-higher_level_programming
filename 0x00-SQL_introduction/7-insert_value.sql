befe7a9aef41a28f2822eccde44647c7dd11d9c6
Douglas <wdmdavison@icloud.com>
2022-10-06 18:47:07 -0700

    upload 0

--- 0x00-SQL_introduction/7-insert_value.sql
+++ 0x00-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- now let's insert a new row into the table
 -- first_table that we made already
-INSERT INTO first_table (id, name) VALUES (89, 'Best School')
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School')
---
+++ 0x01-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+# let's look at some more SQL
