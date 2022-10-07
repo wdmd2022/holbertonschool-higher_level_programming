19a6eead68d414c895f2c9d9e065b9a6d6c53afd
Douglas <wdmdavison@icloud.com>
2022-10-06 18:59:17 -0700

    include flush

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -2,3 +2,4 @@
 -- with all privileges, if he doesn't exist, with name user_0d_1 and pass user_0d_1_pwd
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
 GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
+FLUSH PRIVILEGES;
