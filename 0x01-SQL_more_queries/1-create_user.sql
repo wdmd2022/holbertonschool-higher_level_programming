a444ab4df4395c26972dd32a733b7627984e46d5
Douglas <wdmdavison@icloud.com>
2022-10-06 18:50:55 -0700

    upload 1

---
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- now let's create a user (if they don't already exist) on our server,
+-- with all privileges, if he doesn't exist, with name user_0d_1 and pass user_0d_1_pwd
+CREATE USER IF NOT EXISTS `user_0d_1` IDENTIFIED WITH auth_plugin AS `user_0d_1_pwd`
