7b81df4e81bfe06f2e31c8ae336aed770662dc46
Douglas <wdmdavison@icloud.com>
2022-10-06 21:25:49 -0700

    upload 12

---
+++ 0x01-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- using that same database dump as before, list all shows in the db
+-- each record should display <tv_shows.title> - <tv_show_genres.genre_id>
+-- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- only use SELECT once
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
