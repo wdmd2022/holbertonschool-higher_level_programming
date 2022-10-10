8f0f0d6b7ed3727159a37990a9707934b76d1f41
Douglas <wdmdavison@icloud.com>
2022-10-10 15:52:23 -0700

    fix table name typo

--- 0x01-SQL_more_queries/16-shows_by_genre.sql
+++ 0x01-SQL_more_queries/16-shows_by_genre.sql
@@ -5,5 +5,5 @@
 -- use only one SELECT statement
 SELECT tv_shows.title, tv_genres.name FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.genre_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
