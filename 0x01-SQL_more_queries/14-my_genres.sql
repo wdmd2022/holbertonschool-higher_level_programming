5e8453e9b7cd3de55c9417f6a9eceedc96f32219
Douglas <wdmdavison@icloud.com>
2022-10-10 15:38:51 -0700

    upload 14

--- 0x01-SQL_more_queries/14-my_genres.sql
+++ 0x01-SQL_more_queries/14-my_genres.sql
@@ -2,4 +2,6 @@
 -- reach record should display tv_genres.name, sorted ascending
 -- use only one SELECT statement. tv_shows table only contains 1 record
 -- where title = Dexter, FYI
-SELECT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres
+SELECT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
+WHERE tv_shows.title='Dexter' ORDER BY tv_genres.name ASC;
