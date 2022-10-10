59e456b90fd2972a3f4cc5d0904ad61cf043dccc
Douglas <wdmdavison@icloud.com>
2022-10-10 15:32:07 -0700

    fix typo

--- 0x01-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,6 +5,6 @@
 -- skip genres without any shows linked
 -- sort by descending order of # shows linked
 -- only use one SELECT statement
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows FROM tv_genres
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id ORDER BY number_shows DESC;
