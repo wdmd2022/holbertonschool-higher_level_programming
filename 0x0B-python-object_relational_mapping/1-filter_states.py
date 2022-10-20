3b57e3c55538a6f880148ca2569e65f60639aecf
Douglas <wdmdavison@icloud.com>
2022-10-20 09:34:47 -0700

    add try-except block

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -5,14 +5,17 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    records = cur.fetchall()
-    for record in records:
-        print(record)
-    # Close all cursors
-    cur.close()
-    # Close all databases
-    db.close()
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3])
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+        records = cur.fetchall()
+        for record in records:
+            print(record)
+        # Close cursor
+        cur.close()
+        # Close database
+        db.close()
+    except Exception:
+        quit
