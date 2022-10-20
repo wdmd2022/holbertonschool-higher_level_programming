e182e86becb592188f605074c2058de3d3aa2145
Douglas <wdmdavison@icloud.com>
2022-10-20 10:07:50 -0700

    upload 2 with case check

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
         db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id"
-                    .format(sys.argv[4]))
+        cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER\
+                    BY id".format(sys.argv[4]))
         records = cur.fetchall()
         for record in records:
             print(record)
