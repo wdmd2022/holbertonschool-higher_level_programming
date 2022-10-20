1f48e82e7539722471827e43aa80fecb9e9e374e
Douglas <wdmdavison@icloud.com>
2022-10-20 15:32:14 -0700

    upload 6

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""this module contains a script that lists all cities from the table
-hbtn_0e_0_usa using MySQLdb, sorted by cities.id (asc), and only using
-execute() once. Takes 3 args: mysql username, mysql pass, db name"""
+"""this module contains a script that lists all cities from a state in
+hbtn_0e_0_usa using MySQLdb, sorted by cities.id (asc), safe from injection,
+and only using execute() once. Takes 4 args: mysql username, mysql pass, db
+name, state"""
 
 import MySQLdb
 import sys
@@ -11,12 +12,19 @@ if __name__ == '__main__':
         db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
-        cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-                    INNER JOIN states ON cities.state_id = states.id\
-                    ORDER BY id;")
-        records = cur.fetchall()
-        for record in records:
-            print(record)
+        cur.execute("SELECT cities.name FROM cities LEFT JOIN states ON BINARY\
+                    cities.state_id = states.id WHERE states.id = (SELECT\
+                    id FROM states WHERE name = %s) ORDER BY cities.id",
+                    (sys.argv[4],))
+        rows = cur.fetchall()
+        if len(rows) <= 0:
+            print("")
+            quit
+        for row in rows:
+            if row == rows[-1]:
+                print(row[0])
+            else:
+                print("{}".format(row[0]), end=", ")
         # Close cursor
         cur.close()
         # Close database
