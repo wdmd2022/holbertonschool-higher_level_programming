cf360e15a209445044f8c77b47719619e5351555
Douglas <wdmdavison@icloud.com>
2022-11-02 01:50:36 +0000

    upload fix

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """this module contains a script to fetch a url"""
 
-import urllib.request
+if __name__ == '__main__':
+    import urllib.request
 
-if __name__ == "__ main __":
-    with urllib.request.urlopen(
-        urllib.request.Request("https://intranet.hbtn.io/status")) as req:
-            payoff = req.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(payoff)))
-            print("\t- content: {}".format(payoff))
-            print("\t- utf8 content: {}".format(payoff.decode('utf8')))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as req:
+        payoff = req.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(payoff)))
+        print("\t- content: {}".format(payoff))
+        print("\t- utf8 content: {}".format(payoff.decode('utf-8')))
