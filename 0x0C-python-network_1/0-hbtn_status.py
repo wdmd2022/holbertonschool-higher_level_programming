c3f5358b443084c1fae4b96c3385e7cd66ce9a17
Douglas <wdmdavison@icloud.com>
2022-11-01 18:13:39 -0700

    push to access from new server

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -3,9 +3,11 @@
 
 import urllib.request
 
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    stuff_we_got = response.read()
-    for line in stuff_we_got:
-        print("\t- {}".format(line))
-
+if __name__ == "__ main __":
+    with urllib.request.urlopen(
+        urllib.request.Request("https://intranet.hbtn.io/status")) as req:
+            payoff = req.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(payoff)))
+            print("\t- content: {}".format(payoff))
+            print("\t- utf8 content: {}".format(payoff.decode('utf8')))
