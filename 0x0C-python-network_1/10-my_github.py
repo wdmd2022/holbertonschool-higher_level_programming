c335c060c6cb09a5edb3983fe01e6d88c41a9dc5
Douglas <wdmdavison@icloud.com>
2022-11-06 22:20:10 +0000

    change HTTPBasicAuth import for requests

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -5,8 +5,9 @@ as password) and uses the GitHub API to display your id"""
 if __name__ == "__main__":
     import requests
     from sys import argv
+    from requests.auth import HTTPBasicAuth
 
-    r = requests.get('https://api.github.com/user',
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    logmein = HTTPBasicAuth(argv[1], argv[2])
+    r = requests.get('https://api.github.com/user', auth=logmein)
     uid = r.json()
     print(uid.get('id'))
