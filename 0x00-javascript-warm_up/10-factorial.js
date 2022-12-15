2fae99478ce048e49abec6b224e170199d39b7d2
Douglas <wdmdavison@icloud.com>
2022-12-14 18:14:01 -0800

    upload 10

---
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function factorialize (num) {
+  if ((num === 0) || (isNaN(num))) {
+    return (1);
+  } else {
+    return (num * factorialize(num - 1));
+  }
+}
+console.log(factorialize(parseInt(process.argv[2])));
