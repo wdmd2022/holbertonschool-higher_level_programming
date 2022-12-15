6468f11923726fb78ba9ac0dca74f9971e9223cf
Douglas <wdmdavison@icloud.com>
2022-12-15 06:58:42 -0800

    upload 3

--- 0x01-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,5 +6,15 @@ class Rectangle {
       this.height = h;
     }
   }
+
+  print () {
+    let stringy = '';
+    for (let i = 0; i < this.width; i++) {
+      stringy = stringy + 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(stringy);
+    }
+  }
 }
 module.exports = Rectangle;
