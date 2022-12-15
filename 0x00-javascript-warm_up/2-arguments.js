1ea89654932c15ce089333c0bf0f05ca87c8c91b
Douglas <wdmdavison@icloud.com>
2022-12-14 16:30:01 -0800

    upload 2 for testing

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-let length = process.argv.length
-if (length == 2) {
-    console.log('No argument')
+const length = process.argv.length;
+if (length === 2) {
+  console.log('No argument');
 }
-if (length == 3) {
-    console.log('Argument found')
+if (length === 3) {
+  console.log('Argument found');
 }
-if (length == 4) {
-    console.log('Arguments found')
+if (length === 4) {
+  console.log('Arguments found');
 }
