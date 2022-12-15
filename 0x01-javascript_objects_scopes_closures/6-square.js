892c235459ed4d2e3344330a51ccff3347e2177f
Douglas <wdmdavison@icloud.com>
2022-12-15 08:21:00 -0800

    upload 6 for testing

---
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const Square1 = require('./5-square.js');
+class Square extends Square1 {
+  charPrint (c = 'X') {
+    let stringy = '';
+    for (let i = 0; i < this.width; i++) {
+      stringy = stringy + c;
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(stringy);
+    }
+  }
+}
